e22ab97090ca6c1044c20be3f45e8a4e397e0262
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-08-22 22:02:53 -0300

    Aclaracion archivo prueba

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,2 @@
-#----------------------------------------Ejercicio 1----------------------------------------#----------------------------------------Ejercicio 1----------------------------------------
-# Solicitamos edad al usuario y lo guardamos en la variable "edad"
-edad : int = int(input("Ingrese su edad: "))
-
-# Creamos una variable mayor_de_edad por si en algún momento llegara a cambiar (como de 21 a 18)
-mayor_de_edad = 18
-
-if edad >= mayor_de_edad:
-    print("Es mayor de edad")
-elif edad > 0 and edad < mayor_de_edad:
-    print("Es menor de edad")
-else:
-    print("Valor inválido")
+# En este archivo se realiza la prueba de cada ejercicio que se va desarrollando 
+# para luego copiarlo en el archivo .py original
