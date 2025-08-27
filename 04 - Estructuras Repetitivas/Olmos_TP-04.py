6a50a07ee4d2ca8cf9cd6a9ceacf8b8a19a79ece
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-08-27 16:12:01 -0300

    Ej_2 Resuelto

--- 04 - Estructuras Repetitivas/Olmos_TP-04.py
+++ 04 - Estructuras Repetitivas/Olmos_TP-04.py
@@ -1,3 +1,16 @@
 #----------------------------------------Ejercicio 1----------------------------------------
+# Creamos un bucle for ya que sabemos cuantas veces tiene que repetirse la instrucción y el límite
+# lo fijamos en 101 para que el 100 esté incluido.
 for i in range(101):
     print(i)
+
+#----------------------------------------Ejercicio 2----------------------------------------
+# Pedimos al usuario que ingrese un numero entero, pero lo guardamos como cadena para poder contar
+# sus "dígitos" (que serían los caracteres)
+num : str =input("Ingrese un número entero: ")
+
+# Creamos una variable contador que guarda la cantidad de dígitos y lo mostramos (aunque sería más fácil utilizar la funcion len())
+cont = 0
+while cont < len(num):
+    cont += 1
+print(f"Cantidad de caracteres: {cont}")
