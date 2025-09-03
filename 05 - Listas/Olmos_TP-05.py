44dd4986461f524d48d59959759e8749372cff64
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-09-03 17:45:34 -0300

    Ej_1 Resuelto

---
+++ 05 - Listas/Olmos_TP-05.py
@@ -0,0 +1,8 @@
+#----------------------------------------Ejercicio 1----------------------------------------
+# Creamos la lista
+lista = []
+for i in range(1,100):
+    if i % 4 == 0:
+        lista.append(i)
+# Imprimimos (aunque no se nos pide específicamente)
+print(lista)
