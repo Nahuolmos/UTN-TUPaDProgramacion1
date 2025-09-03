03ac942260e72e6422d937b5996bc54c27a38a62
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-09-03 17:51:25 -0300

    Ej_2 Resuelto

--- 05 - Listas/Olmos_TP-05.py
+++ 05 - Listas/Olmos_TP-05.py
@@ -6,3 +6,10 @@ for i in range(1,100):
         lista.append(i)
 # Imprimimos (aunque no se nos pide específicamente)
 print(lista)
+
+#----------------------------------------Ejercicio 2----------------------------------------
+# Creamos una lista cualquiera
+lista = [1, 2, 3, 4, 5]
+# Pedimos mostrar el elemento "-2" ya que al utilizar el número negativo accedemos al penúltimo
+# elemento, sin necesidad de saber exactamente la longitud de la lista
+print(lista[-2])
