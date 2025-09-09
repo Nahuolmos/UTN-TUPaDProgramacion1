28c0bb17a2b6880da6ffab523ff942c691e4e882
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-09-09 15:55:00 -0300

    Ej_9 Resuelto

--- 05 - Listas/Olmos_TP-05.py
+++ 05 - Listas/Olmos_TP-05.py
@@ -73,3 +73,20 @@ dobles.append(2*15)
 # Imprimimos la lista resultante
 print(dobles)
 
+#----------------------------------------Ejercicio 9----------------------------------------
+# Generamos la lista que se nos da como base
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+
+# Agregar "jugo" a la lista del tercer cliente usando append
+compras[2].append("jugo")
+
+# Reemplazar "fideos" por "tallarines" en la lista del segundo cliente
+compras[1][1] = "tallarines"
+
+# Eliminar "pan" de la lista del primer cliente
+compras[0].remove("pan")
+
+# Imprimimos el resultado
+print
+print(f"Lista final: {compras}")
+
