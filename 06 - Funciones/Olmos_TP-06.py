94bc8bccbe8c5ae75af17f1820725df81a990251
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-15 17:07:16 -0300

    Ej_1 Resuelto

---
+++ 06 - Funciones/Olmos_TP-06.py
@@ -0,0 +1,6 @@
+#----------------------------------------Ejercicio 1----------------------------------------
+# Creamos la función y la nombramos
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+# Llamamos a la función
+imprimir_hola_mundo()
