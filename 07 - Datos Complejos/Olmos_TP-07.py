f79d1ab4c668ab5b518f90eb74c1b176e9b1e567
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-21 14:18:27 -0300

    Ej_2 Resuelto

--- 07 - Datos Complejos/Olmos_TP-07.py
+++ 07 - Datos Complejos/Olmos_TP-07.py
@@ -9,3 +9,8 @@ precios_frutas['Naranja'] = 1200
 precios_frutas['Manzana'] = 1500
 precios_frutas['Pera'] = 2300
 
+#----------------------------------------Ejercicio 2----------------------------------------
+# Actualizamos precios de las frutas del diccionario anterior
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] = 2800
