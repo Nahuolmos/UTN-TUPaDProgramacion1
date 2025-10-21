67c27b9ce9f34307cc97d0261ee2c9286dc87edb
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-21 14:43:39 -0300

    Ej_4 Resuelto

--- 07 - Datos Complejos/Olmos_TP-07.py
+++ 07 - Datos Complejos/Olmos_TP-07.py
@@ -14,3 +14,24 @@ precios_frutas['Pera'] = 2300
 precios_frutas['Banana'] = 1330
 precios_frutas['Manzana'] = 1700
 precios_frutas['Melón'] = 2800
+
+#----------------------------------------Ejercicio 3----------------------------------------
+# Recuperamos en una lissta las keys de nuestro diccionario para ver solo las frutas sin precios
+precios_frutas.keys()
+
+#----------------------------------------Ejercicio 4----------------------------------------
+# Creacion del diccionario contactos
+contactos = {}
+for i in range(1,6):
+    contactos[input("Ingrese el nombre del contacto: ")] = int(input("Ingrese el número telefónico: "))
+    print()
+
+# Consulta dentro del diccionario
+def buscar_por_nombre(nombre_a_buscar):
+    if nombre_a_buscar in contactos:
+        print("El número es: ", contactos[nombre_a_buscar])
+    else:
+        print("No se encontró el contacto buscado")
+
+nombre_a_buscar = input("Ingrese el nombre del contacto a buscar: ")
+buscar_por_nombre(nombre_a_buscar)
