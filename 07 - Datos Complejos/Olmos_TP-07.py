dff247db5d365d7b3acb4f19eb4817660f8ff200
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-21 13:37:24 -0300

    Creacion de 'Datos Complejos' y Ej_1 Resuelto

---
+++ 07 - Datos Complejos/Olmos_TP-07.py
@@ -0,0 +1,11 @@
+#----------------------------------------Ejercicio 1----------------------------------------
+# Creamos el diccionario inicial
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+# Agregamos lo que se nos pide
+# Naranja = 1200 
+# Manzana = 1500
+# Pera = 2300
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+
