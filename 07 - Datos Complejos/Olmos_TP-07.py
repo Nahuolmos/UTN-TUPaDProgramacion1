20f5b092efa551abaa28741c53dd1f73de509bd0
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-25 13:11:31 -0300

    Ej_6 Resuelto

--- 07 - Datos Complejos/Olmos_TP-07.py
+++ 07 - Datos Complejos/Olmos_TP-07.py
@@ -53,3 +53,18 @@ print(palabras_unicas)
 print("Frecuencia de cada palabra:")
 print(frecuencias)
 
+#----------------------------------------Ejercicio 6----------------------------------------
+alumnos = {}
+can_alumnos : int = 3
+for i in range(can_alumnos):
+    nombre = input(f"Ingrese el nombre del alumno {i + 1}: ")
+    notas = []
+    for j in range(3):
+        nota = float(input(f"Ingrese la nota {j + 1} de {nombre}: "))
+        notas.append(nota)
+    alumnos[nombre] = tuple(notas)
+
+print("Promedios de los alumnos:")
+for nombre, notas in alumnos.items():
+    promedio = sum(notas) / len(notas)
+    print(f"{nombre}: {promedio}")
