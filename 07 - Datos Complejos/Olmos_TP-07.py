08d0ecb75af0ff1fe52d69f55572ef0417bb8b83
Nahuolmos <103071584+Nahuolmos@users.noreply.github.com>
2025-10-29 00:49:54 -0300

    Ej_9 Resuelto

--- 07 - Datos Complejos/Olmos_TP-07.py
+++ 07 - Datos Complejos/Olmos_TP-07.py
@@ -129,3 +129,16 @@ while bandera:
     else:
         bandera = False
         print("¡Adiós!")
+
+#----------------------------------------Ejercicio 9----------------------------------------
+agenda = {("sábado", "20:00"): "casamiento", ("sábado", "10:00"): "partido", ("lunes", "8:00"): "examen"}
+
+dias_y_horas = list(agenda.keys())
+print(f"Tienes actividades el {dias_y_horas[0][0]} a las {dias_y_horas[0][1]}, el {dias_y_horas[1][0]} a las {dias_y_horas[1][1]} y el {dias_y_horas[2][0]} a las {dias_y_horas[2][1]}.")
+dia = input("¿Qué actividad desea consultar? Ingrese día: ")
+hora = input("Ingrese hora (ejemplo: 20:00): ")
+dia_y_hora = (dia.lower(), hora)
+if dia_y_hora not in agenda:
+    print("No tienes actividades ese día a esa hora.")
+else:
+    print(f"Tienes {agenda[dia_y_hora]} ese día a esa hora.")
